d6c5044b20538b6b2a2400d791fb87833acdeb63
Tope <castillochristopherjames@gmail.com>
2024-01-15 00:30:33 +0800

    configured db migrations

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,9 @@
 from typing import Annotated
 from fastapi import Header, HTTPException
+from .repo.database import Session
 
 # dependencies goes here
 # add JWT support here
+
+async def get_common_db_session():
+    return Session()
